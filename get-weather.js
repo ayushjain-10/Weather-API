a80d7fc9db8c3124034fb84ce762f6a681249fdd
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-09-19 13:29:46 -0700

    update

--- get-weather.js
+++ get-weather.js
@@ -1,10 +1,11 @@
-function getWeather(apiKey, zip, callback, onError) {
+async function getWeather(apiKey, zip, callback, onError) {
     const units = 'imperial'
     const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-    fetch(path)
-        .then(res => res.json())
-        .then(json => callback(json))
-        .catch(err => onError(err))
+    const res = await fetch(path) // waits for the promise to resolve
+    const json = await res.json() // waits for the promise to resolve
+    // console.log(json)
+    .catch(err => { onError(err) })
+    return callback(json)
 }
 
 export default getWeather; // <-- export the function
