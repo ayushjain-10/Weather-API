488b090fe5c18d28bfc88067887b29c5b57f7443
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-09-19 23:57:36 -0700

    added all challenges

--- get-weather.js
+++ get-weather.js
@@ -1,21 +1,42 @@
-async function getWeather(apiKey, zip, callback) {
-    const units = 'imperial'
-    const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-    const res = await fetch(path) // waits for the promise to resolve
-    const json = await res.json() // waits for the promise to resolve
-    const weatherData = {
-        code: json.cod,
-        temp: json.main.temp,
-        description: json.weather[0].description,
-        name: json.name,
-        feels_like: json.main.feels_like,
-        temp_min: json.main.temp_min,
-        temp_max: json.main.temp_max
-    }
-    // console.log(json)
-    return callback(weatherData)
+// Functions
+function getWeatherByZip(apikey, zip, units = 'imperial') {
+    const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}&units=${units}`
+    return getWeather(path)
+}
+  
+function getWeatherByCity(apikey, city, units = 'imperial') {
+    const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=${units}`
+    return getWeather(path)
+}
+  
+function getWeatherByGeo(apikey, lat, lon, units = 'imperial') {
+    const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}&units=${units}`
+    return getWeather(path)
 }
 
-export default getWeather; // <-- export the function
+async function getWeather(path) {
+        // try something here
+        const res = await fetch(path)
+        const json = await res.json()
+        const weatherData = {
+            code: json.cod,
+            coordLat: json.coord.lat,
+            coordLon: json.coord.lon,
+            temp: json.main.temp,
+            description: json.weather[0].description,
+            name: json.name,
+            feels_like: json.main.feels_like,
+            temp_min: json.main.temp_min,
+            temp_max: json.main.temp_max
+        }
+        return weatherData
+    }
+            
+    // return fetch(path).then(res => res.json())      // connect to server recieve response
+      
 
-// 
+export {
+    getWeatherByZip,
+    getWeatherByCity,
+    getWeatherByGeo
+}
