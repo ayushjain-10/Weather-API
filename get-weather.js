9f78ff8584137b7350c99e1cef771e1c05117bb0
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-10-04 23:44:30 -0700

    update

--- get-weather.js
+++ get-weather.js
@@ -1,42 +1,39 @@
-class Weather{
-    constructor(apikey) {
-        this.apikey = apikey
-      }
-    // Functions
-    getWeatherByZip(apikey, zip, units = 'imperial') {
-        const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}&units=${units}`
-        return getWeather(path)
-    }
-    
-    getWeatherByCity(apikey, city, units = 'imperial') {
-        const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=${units}`
-        return getWeather(path)
-    }
-    
-    getWeatherByGeo(apikey, lat, lon, units = 'imperial') {
-        const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}&units=${units}`
-        return getWeather(path)
-    }
-
-    async getWeather(path) {
-            // try something here
-            const res = await fetch(path)
-            const json = await res.json()
-            const weatherData = {
-                code: json.cod,
-                coordLat: json.coord.lat,
-                coordLon: json.coord.lon,
-                temp: json.main.temp,
-                description: json.weather[0].description,
-                name: json.name,
-                feels_like: json.main.feels_like,
-                temp_min: json.main.temp_min,
-                temp_max: json.main.temp_max
-            }
-            return weatherData
-        }
-                
-        // return fetch(path).then(res => res.json())      // connect to server recieve response
-        
+// Functions
+function getWeatherByZip(apikey, zip, units = 'imperial') {
+    const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}&units=${units}`
+    return getWeather(path)
 }
-export default Weather
+
+function getWeatherByCity(apikey, city, units = 'imperial') {
+    const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=${units}`
+    return getWeather(path)
+}
+
+function getWeatherByGeo(apikey, lat, lon, units = 'imperial') {
+    const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}&units=${units}`
+    return getWeather(path)
+}
+
+async function getWeather(path) {
+        // try something here
+        const res = await fetch(path)
+        const json = await res.json()
+        const weatherData = {
+            code: json.cod,
+            coordLat: json.coord.lat,
+            coordLon: json.coord.lon,
+            temp: json.main.temp,
+            description: json.weather[0].description,
+            name: json.name,
+            feels_like: json.main.feels_like,
+            temp_min: json.main.temp_min,
+            temp_max: json.main.temp_max
+        }
+        return weatherData
+    }
+    // Export functions
+    export {
+        getWeatherByZip,
+        getWeatherByCity,
+        getWeatherByGeo
+    }
